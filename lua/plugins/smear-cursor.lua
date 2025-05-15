faa9cffa3314e0e648bc4477fac3a80413ae3772
zykowal <2570713180@qq.com>
2025-05-16 00:42:07 +0800

    feat: smear cursor plugin

---
+++ lua/plugins/smear-cursor.lua
@@ -0,0 +1,12 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    legacy_computing_symbols_support = true,
+    transparent_bg_fallback_color = "#303030",
+    stiffness = 0.8,
+    trailing_stiffness = 0.5,
+    stiffness_insert_mode = 0.6,
+    trailing_stiffness_insert_mode = 0.6,
+    distance_stop_animating = 0.5,
+  },
+}
