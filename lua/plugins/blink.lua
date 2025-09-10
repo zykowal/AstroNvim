17e78d96401971ca492409566634771395d59a9b
zykowal <2570713180@qq.com>
2025-09-11 02:56:28 +0800

    feat: Split plugin in single file

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,40 @@
+return {
+  "Saghen/blink.cmp",
+  opts = {
+    cmdline = {
+      keymap = {
+        ["<Tab>"] = { "show", "accept" },
+        ["<S-Tab>"] = { "show_and_insert", "select_prev" },
+
+        ["<C-N>"] = { "select_next", "show" },
+        ["<C-P>"] = { "select_prev", "show" },
+        ["<C-J>"] = { "select_next", "fallback" },
+        ["<C-K>"] = { "select_prev", "fallback" },
+
+        ["<C-Y>"] = { "select_and_accept" },
+        ["<C-E>"] = { "cancel" },
+      },
+      completion = { menu = { auto_show = true } },
+    },
+    completion = {
+      menu = {
+        border = "none",
+        scrollbar = false,
+        draw = {
+          columns = { { "label", gap = 1 }, { "label_description", gap = 1 }, { "kind", gap = 1 } },
+        },
+      },
+      documentation = {
+        window = {
+          border = "rounded",
+          scrollbar = false,
+        },
+      },
+    },
+    signature = {
+      window = {
+        border = "none",
+      },
+    },
+  },
+}
---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,14 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  lazy = true,
+  priority = 1000,
+  opts = {
+    transparent_background = true,
+    term_colors = true,
+    float = {
+      transparent = true,
+      solid = false,
+    },
+  },
+}
