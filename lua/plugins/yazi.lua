31648e521af321dac350f5701e977b2e2e5ff696
zykowal <gzy520@qq.com>
2026-01-11 16:05:50 +0800

    feat: Update yazi config.

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,24 +1,46 @@
 return {
   "mikavilpas/yazi.nvim",
-  version = "*",
   event = "VeryLazy",
+  version = "*",
+  cmd = "Yazi",
   dependencies = {
     { "nvim-lua/plenary.nvim", lazy = true },
-  },
-  opts = {
-    open_for_directories = true,
-    open_multiple_tabs = true,
-    floating_window_scaling_factor = 0.8,
-    keymaps = {
-      show_help = "<f1>",
-      open_file_in_vertical_split = "<c-v>",
-      open_file_in_horizontal_split = "<c-s>",
-      change_working_directory = "<c-e>",
+    {
+      "AstroNvim/astrocore",
+      ---@type AstroCoreOpts
+      opts = {
+        mappings = {
+          n = {
+            ["<Leader>e"] = { "<Cmd>Yazi<CR>", desc = "Explorer" },
+            ["<Leader>o"] = { "<Cmd>Yazi cwd<CR>", desc = "Explorer (Cwd)" },
+          },
+          v = {
+            ["<Leader>e"] = { "<Cmd>Yazi<CR>", desc = "Explorer" },
+          },
+        },
+      },
     },
   },
-  keys = {
-    { "<Leader>e", "<cmd>Yazi<cr>",     desc = "Explorer" },
-    { "<Leader>o", "<cmd>Yazi cwd<cr>", desc = "Explorer (Cwd)" },
-  },
-  init = function() vim.g.loaded_netrwPlugin = 1 end,
+  opts = function()
+    local opts = {
+      open_for_directories = true,
+      open_multiple_tabs = true,
+      floating_window_scaling_factor = 0.8,
+      keymaps = {
+        show_help = "<F1>",
+        open_file_in_vertical_split = "<c-v>",
+        open_file_in_horizontal_split = "<c-s>",
+        change_working_directory = "<c-e>",
+      },
+    }
+
+    if pcall(require, "snacks") then
+      opts.integrations = {
+        grep_in_directory = "snacks.picker",
+        grep_in_selected_files = "snacks.picker",
+      }
+    end
+
+    return opts
+  end,
 }
