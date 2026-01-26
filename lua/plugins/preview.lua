5e18d813ade8f665544d84f5494ecfa50cc3f9e8
zykowal <gzy520@qq.com>
2026-01-26 19:34:17 +0800

    feat: Update preview setup

--- lua/plugins/preview.lua
+++ lua/plugins/preview.lua
@@ -4,11 +4,11 @@ return {
   config = true,
 
   opts = {
-    width = vim.o.columns,    -- Width of the floating window
-    height = 16,              -- Height of the floating window
-    default_mappings = false, -- Bind default mappings
-    references = {            -- Configure the telescope UI for slowing the references cycling window.
-      provider = "fzf_lua",   -- telescope|fzf_lua|snacks|mini_pick|default
+    width = vim.o.columns,      -- Width of the floating window
+    height = vim.o.lines * 0.5, -- Height of the floating window
+    default_mappings = false,   -- Bind default mappings
+    references = {              -- Configure the telescope UI for slowing the references cycling window.
+      provider = "fzf_lua",     -- telescope|fzf_lua|snacks|mini_pick|default
     },
   },
 
