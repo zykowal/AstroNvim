a1bab82e79116ba7f025d2b50f2d9cc32ee16c12
zykowal <gzy520@qq.com>
2026-02-06 01:14:03 +0800

    feat: Update blink completion icons.

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -34,6 +34,55 @@ return {
 			},
 			opts = {},
 		},
+		{
+    "onsails/lspkind.nvim",
+    event = "InsertEnter",
+    config = function()
+        require("lspkind").init({
+            symbol_map = {
+                Array = "  ",
+                Boolean = " 󰨙 ",
+                Class = " 󰯳 ",
+                Codeium = " 󰘦 ",
+                Color = " 󰰠 ",
+                Control = "  ",
+                Collapsed = " > ",
+                Constant = " 󰯱 ",
+                Constructor = "  ",
+                Copilot = "  ",
+                Enum = " 󰯹 ",
+                EnumMember = "  ",
+                Event = "  ",
+                Field = "  ",
+                File = "  ",
+                Folder = "  ",
+                Function = " 󰡱 ",
+                Interface = " 󰰅 ",
+                Key = "  ",
+                Keyword = " 󱕴 ",
+                Method = " 󰰑 ",
+                Module = " 󰆼 ",
+                Namespace = " 󰰔 ",
+                Null = "  ",
+                Number = " 󰰔 ",
+                Object = " 󰲟 ",
+                Operator = "  ",
+                Package = " 󰰚 ",
+                Property = " 󰲽 ",
+                Reference = " 󰰠 ",
+                Snippet = "  ",
+                String = "  ",
+                Struct = " 󰰣 ",
+                TabNine = " 󰏚 ",
+                Text = " 󱜥 ",
+                TypeParameter = " 󰰦 ",
+                Unit = " 󱜥 ",
+                Value = "  ",
+                Variable = " 󰫧 ",
+            },
+        })
+    end,
+}
 	},
 
 	-- use a release tag to download pre-built binaries
@@ -123,8 +172,35 @@ return {
 				scrollbar = false,
 				draw = {
 					treesitter = { "lsp" },
-					columns = { { "label", "label_description", gap = 1 }, { "kind", gap = 1 } },
-				},
+          columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
+          components = {
+            kind_icon = {
+              text = function(ctx)
+                if ctx.source_name ~= "Path" then
+                  return (require("lspkind").symbol_map[ctx.kind] or "") .. ctx.icon_gap
+                end
+                local is_unknown_type = vim.tbl_contains({ "link", "socket", "fifo", "char", "block", "unknown" },
+                  ctx.item.data.type)
+                local mini_icon, _ = require("mini.icons").get(
+                  is_unknown_type and "os" or ctx.item.data.type,
+                  is_unknown_type and "" or ctx.label
+                )
+                return (mini_icon or ctx.kind_icon) .. ctx.icon_gap
+              end,
+              highlight = function(ctx)
+                if ctx.source_name ~= "Path" then return ctx.kind_hl end
+
+                local is_unknown_type = vim.tbl_contains({ "link", "socket", "fifo", "char", "block", "unknown" },
+                  ctx.item.data.type)
+                local mini_icon, mini_hl = require("mini.icons").get(
+                  is_unknown_type and "os" or ctx.item.data.type,
+                  is_unknown_type and "" or ctx.label
+                )
+                return mini_icon ~= nil and mini_hl or ctx.kind_hl
+              end,
+            }
+          }
+        },
 			},
 			documentation = {
 				auto_show = true,
